1478aefc0cce80973f2ba44be23fac3e030f0255
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-09-16 16:01:57 -0500

    add files for vercel

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(generalLimiter);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(flash());
 app.use(
   session({
